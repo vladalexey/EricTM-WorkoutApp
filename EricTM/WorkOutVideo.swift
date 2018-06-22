8823f372d3d3885d0e5b03a89f41b3c4cd40a286
Phan Quân <qmp23@drexel.edu>
2018-06-22 15:32:05 +0700

    Added WOVideo base class

---
+++ EricTM/WorkOutVideo.swift
@@ -0,0 +1,16 @@
+//
+//  WorkOutVideo.swift
+//  EricTM
+//
+//  Created by Phan Quân on 6/22/18.
+//  Copyright © 2018 Phan Quân. All rights reserved.
+//
+
+import UIKit
+
+//MARK: Properties
+
+var name: String
+var video: UIVideo
+
+
