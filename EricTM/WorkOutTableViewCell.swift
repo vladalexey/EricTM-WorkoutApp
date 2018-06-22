aae9a52ca26c26e923cd906dda24f3a2bb71bf7a
Phan Quân <qmp23@drexel.edu>
2018-06-22 15:21:18 +0700

    Major Updates on Main screen
    Created Main screen (TableView)
    Created TableViewCell + TableViewController

---
+++ EricTM/WorkOutTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  WorkOutTableViewCell.swift
+//  EricTM
+//
+//  Created by Phan Quân on 6/22/18.
+//  Copyright © 2018 Phan Quân. All rights reserved.
+//
+
+import UIKit
+
+class WorkOutTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
