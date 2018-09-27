ec347d900db127070c503de953de1893c79b4b5e
lephannam <namlp12@gmail.com>
2018-09-27 17:43:27 +0700

    Fix status bar color bug

---
+++ Extensions/UINavigationControllerExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UINavigationControllerExtension.swift
+//  EricTM
+//
+//  Created by 2Byte on 9/27/18.
+//  Copyright © 2018 Phan Quân. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    open override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
