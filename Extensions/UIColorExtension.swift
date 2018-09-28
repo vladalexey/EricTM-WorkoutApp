697d857ef39a62b123a501485842ec118d504133
lephannam <namlp12@gmail.com>
2018-09-28 16:34:19 +0700

    Refactor Extensions file

---
+++ Extensions/UIColorExtension.swift
@@ -0,0 +1,40 @@
+//
+//  UIColorExtension.swift
+//  EricTM
+//
+//  Created by 2Byte on 9/28/18.
+//  Copyright © 2018 Phan Quân. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor
+{
+    static let backgroundColor = UIColor(
+        red: 0.17,
+        green: 0.17,
+        blue: 0.17,
+        alpha: 1.0
+    )
+    
+    static let backgroundColorToolbar = UIColor(
+        red: 0.13,
+        green: 0.13,
+        blue: 0.13,
+        alpha: 1.0
+    )
+    
+    static let backgroundColorCell = UIColor(
+        red: 0.25,
+        green: 0.25,
+        blue: 0.25,
+        alpha: 1.0
+    )
+    
+    static let backgroundTabBarColor = UIColor(
+        red: 0.05,
+        green: 0.05,
+        blue: 0.05,
+        alpha: 1.0
+    )
+}
---
+++ Extensions/UIImageExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageExtension.swift
+//  EricTM
+//
+//  Created by 2Byte on 9/28/18.
+//  Copyright © 2018 Phan Quân. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    var noir: UIImage? {
+        let context = CIContext(options: nil)
+        guard let currentFilter = CIFilter(name: "CIPhotoEffectNoir") else { return nil }
+        currentFilter.setValue(CIImage(image: self), forKey: kCIInputImageKey)
+        if let output = currentFilter.outputImage,
+            let cgImage = context.createCGImage(output, from: output.extent) {
+            return UIImage(cgImage: cgImage, scale: scale, orientation: imageOrientation)
+        }
+        return nil
+    }
+}
